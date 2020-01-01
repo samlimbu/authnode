53634974b664c79528dead16554ac50c7121c296
samlimbu@hotmail.com <samlimbu@hotmail.com>
2020-01-01 14:33:37 +0545

    first commit

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ app.post('/test', function(req,res,next){
 
 app.use('/users',users);
 //app.use('/category', categoryRouter);
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname,'public/index.html'));
-})
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname,'public/index.html'));
+// })
 app.get('/', (req, res, next)=>{
      res.send('invalid endpoint');
 });
