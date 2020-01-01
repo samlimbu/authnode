a59e066f19b386401226709bd3c2f1b4cdce6d2e
samlimbu@hotmail.com <samlimbu@hotmail.com>
2020-01-01 23:17:28 +0545

    modified for fpl backend proxy

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const categoryRouter = require('./routes/category');
-const users= require('./routes/users');
+const users = require('./routes/users');
+const https = require('https');
+
+
+
+
+
 
 mongoose.connect(config.database);
 //on connection
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
      console.log('Connected to databse ' + config.database);
 })
 
-const app=express(config.database);
+const app = express(config.database);
 app.use(cors());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 //passoport middleware
@@ -24,8 +30,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
 
-app.get('/test', function(req,res,next){
-   
+app.get('/test', function (req, res, next) {
+
      console.log('*****test***********');
      console.log(req);
      console.log('>>>>>>>>>>>>>>>>>>>>');
@@ -35,7 +41,7 @@ app.get('/test', function(req,res,next){
      //res.send('ok test');
      res.send(req.headers);
 });
-app.post('/test', function(req,res,next){
+app.post('/test', function (req, res, next) {
      console.log('***************************************************');
      console.log(req);
      console.log('>>>>>>>>>>>>>>>>>>>>');
@@ -43,20 +49,49 @@ app.post('/test', function(req,res,next){
      console.log(req.params);
      console.log(req.query);
      console.log(req.body);
-    // res.send('ok post');
+     // res.send('ok post');
      res.send(req.headers);
 });
 
-app.use('/users',users);
+app.use('/users', users);
 //app.use('/category', categoryRouter);
 // app.get('*', (req, res)=>{
 //     res.sendFile(path.join(__dirname,'public/index.html'));
 // })
-app.get('/', (req, res, next)=>{
+
+app.get('/fpl', function (req, res, next) {
+
+     console.log('*****fpl***********');
+     const options = {
+          hostname: 'https://fantasy.premierleague.com/api/bootstrap-static/',
+          method: 'GET'
+     }
+     https.get('https://fantasy.premierleague.com/api/bootstrap-static/', (resp) => {
+          let data = '';
+
+          // A chunk of data has been recieved.
+          resp.on('data', (chunk) => {
+               data += chunk;
+          });
+
+          // The whole response has been received. Print out the result.
+          resp.on('end', () => {
+               console.log(JSON.parse(data).explanation);
+               res.send(data);
+          });
+
+     }).on("error", (err) => {
+          console.log("Error: " + err.message);
+     });
+
+     //req.end()
+
+});
+app.get('/', (req, res, next) => {
      res.send('invalid endpoint');
 });
 //heroku port: process.env.PORT || 8080
 const port = process.env.PORT || 4000 || 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
      console.log('server started on port' + port);
 });
