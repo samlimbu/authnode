c0c88e30eec59dde38859cb0190cc1d00b4a2515
samlimbu@hotmail.com <samlimbu@hotmail.com>
2020-01-03 17:13:27 +0545

    modify register

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.post('/register', (req, res, next) => {
         username: req.body.username,
         password: req.body.password
     });
-    User.addUser(newUser, (err, user) => {
+    User.addUser(req.body, (err, user) => {
         if (err) {
             res.json({ success: false, msg: 'failed to register user' });
         }
